3cf78f54128d6083eb71e1bc8a9b5248c9c5999b
Jose Martinez <sotero.valdez@gmail.com>
2017-08-27 23:09:06 -0500

    auth, modules and restructure

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+import { DataStorageService } from "app/shared/data-storage.service";
+import { AuthService } from "app/auth/auth.service";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+  constructor(private dataStorageService: DataStorageService,
+    private authService: AuthService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes().subscribe(
+      (response: Response) => {
+        console.log(response);
+      }
+    );
+  }
+
+  onGetData() {
+    this.dataStorageService.getRecipes();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+}
